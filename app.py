8d85acb40f8e8dcebfd76e00001cfb04dfc1bec0
shiddatt <125581146+shiddatt@users.noreply.github.com>
2023-02-17 12:36:00 +0000

    I added a Flask project

---
+++ app.py
@@ -0,0 +1,12 @@
+print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, Shiddat!</p>"
+
+print(__name__)
+if __name__== '__main__':
+ app.run(host='0.0.0.0', debug=True)
